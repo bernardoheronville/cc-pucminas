3716a444d73cec4e0e790200a9f928daa70fbf16
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-08-05 21:26:23 -0300

    Exercicio palindromo em c iniciado

---
+++ segundoPeriodo/AEDS-2/verde/palindromoEmC/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool palindromo(char *palavra)
+{
+	int inicio = 0;
+	int final = strlen(palavra) - 1;
+	while (inicio < final)
+	{
+		if (palavra[inicio] != palavra[final])
+		{
+			return false;
+		}
+		inicio++;
+		final--;
+	}
+	return true;
+}
+
+int main()
+{
+	char palavra[100];
+	scanf("%s", palavra);
+	while (strcmp(palavra, "FIM") != 0)
+	{
+		if (palindromo(palavra) == true)
+			printf("SIM\n");
+		else
+			printf("NAO\n");
+		scanf("%s", palavra);
+	}
+	return 0;
+}
