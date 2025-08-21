4a2dcd65b65bc3ed6e432c57daaf03ab7f5b2175
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-08-21 14:54:45 -0300

    Tp 01 ex 02 iniciado, falta mexer com os espacos

--- segundoPeriodo/AEDS-2/tps/tp01/ex02/main.c
+++ segundoPeriodo/AEDS-2/tps/tp01/ex02/main.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <wchar.h>
+#include <locale.h>
 
-bool palindromo(char palavra[], int inicio, int fim) {
+bool palindromo(wchar_t palavra[], int inicio, int fim) {
 	if(inicio >= fim) return true;
 	else if(palavra[inicio] != palavra[fim]) return false;
 	else return palindromo(palavra, inicio + 1, fim - 1);
 }
 
-int my_strlen(char palavra[]) {
+int my_strlen(wchar_t palavra[]) {
 	int contador = 0;
-	while(palavra[contador] != '\0') {
+	while(palavra[contador] != L'\0') {
 		contador++;
 	}
 	return contador;
 }
 
-bool my_strcmp(char palavra[]) {
-	if(palavra[0] == 'F' && palavra[1] == 'I' && palavra[2] == 'M' && palavra[3] == '\0') return false;
-	else return true;
-}
-
 int main() {
-	char palavra[255];
-	scanf("%s",palavra);
-	while(my_strcmp(palavra)) {
+	setlocale(LC_ALL, ""); //habilita UTF-8 no sistema
+	wchar_t palavra[255];
+	wscanf(L"%ls", palavra);
+	while(wcscmp(palavra, L"FIM")) {
 		if(palindromo(palavra, 0, my_strlen(palavra) - 1)) {
 			printf("SIM\n");
 		}
 		else {
 			printf("NAO\n");
 		}
-		scanf("%s",palavra);
+		swscanf(L"%ls", palavra);
 	}
 	return 0;
 }
