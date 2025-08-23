204d61081656b4989c12fb5da95028eda1f2efff
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-08-23 11:55:24 -0300

    Tp 01 ex 01 resolvido, saida %100

--- segundoPeriodo/AEDS-2/tps/tp01/ex01/Main.java
+++ segundoPeriodo/AEDS-2/tps/tp01/ex01/Main.java
@@ -1,6 +1,8 @@
-import java.util.Scanner;
+import java.util.Scanner; // importando a biblioteca scanner
 
-public class Main {
+public class Main { // class main
+
+	//funcao para retorna true or false se a string eh um palindromo
 	public static boolean palindromo(String palavra) {
 		int inicio = 0, fim = palavra.length() - 1;
 		while(inicio < fim) {
@@ -12,6 +14,8 @@ public class Main {
 		}
 		return true;
 	}
+
+	// main
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		String palavra = sc.nextLine();
