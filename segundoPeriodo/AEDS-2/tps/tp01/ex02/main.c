11020a71d4a78f50fa49d7ac48fdd9e9680f8b6e
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-08-23 11:35:50 -0300

    Tp 01 ex 02 resolvido, saida dando %99.5

--- segundoPeriodo/AEDS-2/tps/tp01/ex02/main.c
+++ segundoPeriodo/AEDS-2/tps/tp01/ex02/main.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#include <wchar.h>
-#include <locale.h>
 
-bool palindromo(wchar_t palavra[], int inicio, int fim) {
+//funcao para retorna true or false se a string eh um palindromo
+
+bool palindromo(char *palavra, int inicio, int fim) {
 	if(inicio >= fim) return true;
-	else if(palavra[inicio] != palavra[fim]) return false;
+	else if(*(palavra + inicio) != *(palavra + fim)) return false;
 	else return palindromo(palavra, inicio + 1, fim - 1);
 }
 
-int my_strlen(wchar_t palavra[]) {
+//funcao para retornar um inteiro com o tamanho da string
+
+int my_strlen(char *palavra) {
 	int contador = 0;
-	while(palavra[contador] != L'\0') {
+	while(*(palavra + contador) != '\0') {
 		contador++;
 	}
 	return contador;
 }
 
+//funcao para retorna valor true or false se as string sao iguais
+
+bool my_strcmp(char *palavra1, char *palavra2) {
+	int i = 0;
+	while(*(palavra1 + i) != '\0' && *(palavra2 + i) != '\0') {
+		if(*(palavra1 + i) != *(palavra2 + i)) return false; 
+		i++;
+	}
+	return (*(palavra1 + i) == *(palavra2 + i));
+}
+
+// main
+
 int main() {
-	setlocale(LC_ALL, ""); //habilita UTF-8 no sistema
-	wchar_t palavra[255];
-	wscanf(L"%ls", palavra);
-	while(wcscmp(palavra, L"FIM")) {
+	char *palavra = (char*)malloc(256*sizeof(char));
+	scanf(" %[^\n]", palavra);
+	while(!my_strcmp(palavra, "FIM")) {
 		if(palindromo(palavra, 0, my_strlen(palavra) - 1)) {
 			printf("SIM\n");
 		}
 		else {
 			printf("NAO\n");
 		}
-		swscanf(L"%ls", palavra);
+		scanf(" %[^\n]", palavra);
 	}
+	free(palavra);
 	return 0;
 }
