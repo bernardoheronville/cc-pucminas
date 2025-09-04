99a6aed292dd37586af9d3d2111647b7d7c50f36
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-09-03 21:21:55 -0300

    Ex 01 Tp 02 faltando ajustes

--- segundoPeriodo/AEDS-2/tps/tp02/ex01/Main.java
+++ segundoPeriodo/AEDS-2/tps/tp02/ex01/Main.java
@@ -1,136 +1,156 @@
 import java.util.Scanner;
- 
+
 class Pilha {
-   private String array[];
-   private int topo = -1;
+    private String array[];
+    private int topo = -1;
 
-   public Pilha() {
-       array = new String[100];
-   }
+    public Pilha() {
+        this(200); 
+    }
 
-   public Pilha(int tam) {
-       array = new String[tam];
-   }
+    public Pilha(int tam) {
+        array = new String[tam];
+    }
 
-   public void inserirFinal(String str) {
-       topo++;
-       array[topo] = str;
-   }
-
-   public void removerFinal() {
-       topo--;
-   }
-
-   public int resolve() {
-    int[] aux = new int[100];
-    int topoAux = -1;
-
-    for (int i = 0; i <= topo; i++) {
-        String token = array[i];
-
-        if (Main.my_strcmp(token, "0") || Main.my_strcmp(token, "1")) {
-            topoAux++;
-            aux[topoAux] = Integer.parseInt(token);
-        } 
-        else if (Main.my_strcmp(token, "not")) {
-            int a = aux[topoAux--];
-            topoAux++;
-            aux[topoAux] = (a == 0 ? 1 : 0);
-        } 
-        else if (Main.my_strcmp(token, "and")) {
-            int b = aux[topoAux--];
-            int a = aux[topoAux--];
-            topoAux++;
-            aux[topoAux] = (a == 1 && b == 1 ? 1 : 0);
-        } 
-        else if (Main.my_strcmp(token, "or")) {
-            int b = aux[topoAux--];
-            int a = aux[topoAux--];
-            topoAux++;
-            aux[topoAux] = (a == 1 || b == 1 ? 1 : 0);
+    public void inserirFinal(String str) {
+        if (topo < array.length - 1) {
+            topo++;
+            array[topo] = str;
         }
     }
 
-    return aux[topoAux]; 
-	}
+    public void removerFinal() {
+        if (topo >= 0) {
+            topo--;
+        }
+    }
 
+    public int resolve() {
+        int[] aux = new int[200];
+        int topoAux = -1;
 
-   public void mostrar() {
-       for(int i = 0; i <= topo; i++) {
-           System.out.println(array[i]);
-       }
-   }
-}
-    public class Main {
-        // Funcao para retornar true or false se as string sao iguais.
-        public static boolean my_strcmp(String frase1, String frase2) {
-            boolean resp = true;
-            if(frase1.length() == frase2.length()) {
-                for(int i = 0; i < frase1.length(); i++) {
-                    if(frase1.charAt(i) != frase2.charAt(i)) {
-                        resp = false;
-                        i = frase1.length();
-                    }
+        for (int i = 0; i <= topo; i++) {
+            String elemento = array[i];
+
+            if (Main.my_strcmp(elemento, "0") || Main.my_strcmp(elemento, "1")) {
+                topoAux++;
+                aux[topoAux] = Integer.parseInt(elemento);
+            } else if (Main.my_strcmp(elemento, "not")) {
+                int operando = aux[topoAux--];
+                topoAux++;
+                if (operando == 0) {
+                    aux[topoAux] = 1;
+                } else {
+                    aux[topoAux] = 0;
+                }
+            } else {
+            int op2 = aux[topoAux--]; 
+            int op1 = aux[topoAux--]; 
+            int resultado = 0;
+            if (Main.my_strcmp(elemento, "and")) {
+                if (op1 == 1 && op2 == 1) {
+                    resultado = 1;
+                } else {
+                    resultado = 0;
+                }
+            } else if (Main.my_strcmp(elemento, "or")) {
+                if (op1 == 1 || op2 == 1) {
+                    resultado = 1;
+                } else {
+                    resultado = 0;
                 }
             }
-            else {
-                resp = false;
-            }
-            return resp;
+            topoAux++;
+            aux[topoAux] = resultado;
         }
-       
-        // Funcao para formatar
-        public static String formatacao(String frase, int vals[]) {
-            String formatado = new String();
-            formatado = "";
-            for(int i = 0, j = 0; i < frase.length(); i++) {
-                char caracter = frase.charAt(i);
-                if(caracter != ' ' && caracter != ',') {
-                    if(!(caracter >= 'A' && caracter <= 'Z')) {
-                        formatado += caracter;
-                    }
-                    else {
+    }
+    return aux[topoAux];
+}
+
+    public void mostrar() {
+        for (int i = 0; i <= topo; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
+
+public class Main {
+    
+    // Funcao para retornar true or false se as string sao iguais.
+	public static boolean my_strcmp(String palavra1, String palavra2) {
+		boolean resp = true;
+		if(palavra1.length() == palavra2.length()) {
+			for(int i = 0; i < palavra1.length(); i++) {
+				if(palavra1.charAt(i) != palavra2.charAt(i)) {
+					resp = false;
+					i = palavra1.length();
+				}
+			}
+		}
+		else {
+			resp = false;
+		}
+		return resp;
+	}
+
+    // Funcao para formatar
+    public static String formatacao(String frase, int vals[]) {
+        String formatado = ""; 
+        for(int i = 0, j = 0; i < frase.length(); i++) {
+            char caracter = frase.charAt(i);
+            if (caracter != ' ' && caracter != ',' && caracter != '(' && caracter != ')') {
+                if (!(caracter >= 'A' && caracter <= 'Z')) {
+                    formatado += caracter;
+                } else {
+                    if (j < vals.length) {
                         formatado += vals[j];
                         j++;
                     }
                 }
             }
-			return formatado;
         }
+    return formatado;
+    }
 
-		public static void separador(String str, Pilha pilha) {
-    		int i = 0;
-    		String aux = "";
-   			while (i < str.length()) {
-        		char c = str.charAt(i);
-       			if (c >= 'a' && c <= 'z') { 
-            		aux += c;
-            		if (my_strcmp(aux, "and") || my_strcmp(aux, "not") || my_strcmp(aux, "or")) {
-						pilha.inserirFinal(aux);
-                		aux = ""; 
-           	 		}
-        		}
-				else if (c == '0' || c == '1') {
-            		pilha.inserirFinal(String.valueOf(c));
-        		} 
-        		i++;
-    		}
-		}
-
-   
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-			Pilha pilha = new Pilha();
-            int n = sc.nextInt();
-            int vals[] = new int[n];
-            for(int i = 0; i < n; i++) {
-                vals[i] = sc.nextInt();
+    // Procedimento para retirar caracteres que nao vao ser usados e trocar os valores pelos indices
+    public static void separador(String str, Pilha pilha) {
+        int i = 0;
+        while (i < str.length()) {
+            char c = str.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                String aux = "";
+                while (i < str.length() && str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                    aux += str.charAt(i);
+                    i++;
+                }
+                pilha.inserirFinal(aux);
+            } else if (c == '0' || c == '1') {
+                pilha.inserirFinal(String.valueOf(c));
+                i++;
+            } else {
+                i++;
             }
-            String frase = new String();
-            frase = sc.nextLine();
-            frase = formatacao(frase, vals);
-			separador(frase, pilha);
-			System.out.println(pilha.resolve());
-            sc.close();
         }
     }
+
+    //Main
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n != 0) {
+            int vals[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                vals[i] = sc.nextInt();
+            }
+            sc.nextLine();
+            String frase = sc.nextLine();
+            frase = formatacao(frase, vals);
+            Pilha pilha = new Pilha();
+            separador(frase, pilha);
+            int resultado = pilha.resolve();
+            System.out.println(resultado);
+            n = sc.nextInt();
+        }
+        sc.close();
+    }
+}
