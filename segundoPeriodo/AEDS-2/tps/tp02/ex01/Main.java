bef0f68cfedddb955a0d7cde3f7e631402ec70f3
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-09-04 23:19:55 -0300

    Tp 02 ex 01 resolvido

--- segundoPeriodo/AEDS-2/tps/tp02/ex01/Main.java
+++ segundoPeriodo/AEDS-2/tps/tp02/ex01/Main.java
@@ -1,154 +1,128 @@
 import java.util.Scanner;
 
-class Pilha {
-    private String array[];
-    private int topo = -1;
-
-    public Pilha() {
-        this(200); 
-    }
-
-    public Pilha(int tam) {
-        array = new String[tam];
-    }
-
-    public void inserirFinal(String str) {
-        if (topo < array.length - 1) {
-            topo++;
-            array[topo] = str;
-        }
-    }
-
-    public void removerFinal() {
-        if (topo >= 0) {
-            topo--;
-        }
-    }
-
-    public int resolve() {
-        int[] aux = new int[200];
-        int topoAux = -1;
-
-        for (int i = 0; i <= topo; i++) {
-            String elemento = array[i];
-
-            if (Main.my_strcmp(elemento, "0") || Main.my_strcmp(elemento, "1")) {
-                topoAux++;
-                aux[topoAux] = Integer.parseInt(elemento);
-            } else if (Main.my_strcmp(elemento, "not")) {
-                int operando = aux[topoAux--];
-                topoAux++;
-                if (operando == 0) {
-                    aux[topoAux] = 1;
-                } else {
-                    aux[topoAux] = 0;
-                }
-            } else {
-            int op2 = aux[topoAux--]; 
-            int op1 = aux[topoAux--]; 
-            int resultado = 0;
-            if (Main.my_strcmp(elemento, "and")) {
-                if (op1 == 1 && op2 == 1) {
-                    resultado = 1;
-                } else {
-                    resultado = 0;
-                }
-            } else if (Main.my_strcmp(elemento, "or")) {
-                if (op1 == 1 || op2 == 1) {
-                    resultado = 1;
-                } else {
-                    resultado = 0;
-                }
-            }
-            topoAux++;
-            aux[topoAux] = resultado;
-        }
-    }
-    return aux[topoAux];
-}
-
-    public void mostrar() {
-        for (int i = 0; i <= topo; i++) {
-            System.out.println(array[i]);
-        }
-    }
-}
-
 public class Main {
     
-    // Funcao para retornar true or false se as string sao iguais.
-	public static boolean my_strcmp(String palavra1, String palavra2) {
-		boolean resp = true;
-		if(palavra1.length() == palavra2.length()) {
-			for(int i = 0; i < palavra1.length(); i++) {
-				if(palavra1.charAt(i) != palavra2.charAt(i)) {
-					resp = false;
-					i = palavra1.length();
-				}
-			}
-		}
-		else {
-			resp = false;
-		}
-		return resp;
-	}
-
-    // Funcao para formatar
+    // Substitui letras maiúsculas pelos valores e retira espacos e virgulas.
     public static String formatacao(String frase, int vals[]) {
-        String formatado = ""; 
-        for(int i = 0, j = 0; i < frase.length(); i++) {
-            char caracter = frase.charAt(i);
-            if (caracter != ' ' && caracter != ',' && caracter != '(' && caracter != ')') {
-                if (!(caracter >= 'A' && caracter <= 'Z')) {
-                    formatado += caracter;
-                } else {
-                    if (j < vals.length) {
-                        formatado += vals[j];
-                        j++;
-                    }
+        String formatado = "";
+        for(int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+            if(c != ' ' && c != ',') {
+                if(c >= 'A' && c <= 'Z') {
+                        formatado += vals[c - 'A'];
+                } 
+                else {
+                    formatado += c;
                 }
             }
         }
-    return formatado;
+        return formatado;
     }
 
-    // Procedimento para retirar caracteres que nao vao ser usados e trocar os valores pelos indices
-    public static void separador(String str, Pilha pilha) {
-        int i = 0;
-        while (i < str.length()) {
-            char c = str.charAt(i);
-            if (c >= 'a' && c <= 'z') {
-                String aux = "";
-                while (i < str.length() && str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
-                    aux += str.charAt(i);
-                    i++;
-                }
-                pilha.inserirFinal(aux);
-            } else if (c == '0' || c == '1') {
-                pilha.inserirFinal(String.valueOf(c));
-                i++;
-            } else {
-                i++;
+    // Extrai o operador (not, and, or) antes do '('.
+    public static String extracaoOperadores(String frase, int index) {
+        String str = "";
+        while(index > 0 && frase.charAt(index - 1) >= 'a' && frase.charAt(index - 1) <= 'z') {
+            index--;
+            str += frase.charAt(index);
+        }
+        String operadores = "";
+        for(int j = str.length() - 1; j >= 0; j--) {
+            operadores += str.charAt(j);
+        }
+        return operadores;
+    }
+
+    // Extrai os parâmetros (0 ou 1) dentro dos parênteses.
+    public static int[] extracaoParametros(String frase, int index) {
+        String str = "";
+        while(index < frase.length() - 1 && frase.charAt(index + 1) != ')') {
+            index++;
+            char c = frase.charAt(index);
+            if (c == '0' || c == '1') {
+                str += c;
             }
         }
+        int params[] = new int[str.length()];
+        for(int i = 0; i < str.length(); i++) {
+            params[i] = str.charAt(i) - '0';
+        }
+        return params;
+    }
+
+    // Atualiza a expressão substituindo a subexpressão pelos resultados.
+    public static String atualizar(String frase, int index, String atr) {
+        String str = "";
+        for(int i = 0; i < index; i++) {
+            str += frase.charAt(i);
+        }
+        for(int i = index; i < frase.length(); i++) {
+            if(frase.charAt(i) == ')') {
+                str += atr;
+                i++;
+                while(i < frase.length()) {
+                    str += frase.charAt(i);
+                    i++;
+                }
+            }
+        }
+        return str;
+    }
+
+    // Resolve a operação booleana (not, and, or).
+    public static int resolver(String operator, int[] params) {
+        int resp = 0;
+        switch(operator) {
+            case "not":
+                if(params[0] == 1) {
+                    resp = 0;
+                }
+                else {
+                    resp = 1;
+                }
+                break;
+            case "or":
+                resp = 0;
+                for(int i = 0; i < params.length; i++) {
+                    if (params[i] == 1) {
+                        resp = 1;
+                        i = params.length;
+                    }
+                }
+                break;
+            case "and":
+                resp = 1;
+                for(int i = 0; i < params.length; i++) {
+                    if (params[i] == 0) {
+                        resp = 0;
+                        i = params.length;
+                    }
+                }
+                break;
+        }
+        return resp;
     }
 
     //Main
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while (n != 0) {
-            int vals[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                vals[i] = sc.nextInt();
+        while(n != 0) {
+            int valores[] = new int[n];
+            for(int i = 0; i < n; i++) {
+                valores[i] = sc.nextInt();
             }
-            sc.nextLine();
-            String frase = sc.nextLine();
-            frase = formatacao(frase, vals);
-            Pilha pilha = new Pilha();
-            separador(frase, pilha);
-            int resultado = pilha.resolve();
-            System.out.println(resultado);
+            String linha = sc.nextLine();
+            String expressao = formatacao(linha, valores);
+            for(int i = expressao.length() - 1; i >= 0; i--) {
+                if(expressao.charAt(i) == '(') {
+                    String operador = extracaoOperadores(expressao, i);
+                    int params[] = extracaoParametros(expressao, i);
+                    i -= operador.length(); 
+                    expressao = atualizar(expressao, i, Integer.toString(resolver(operador, params)));
+                }
+            }
+            System.out.println(expressao);
             n = sc.nextInt();
         }
         sc.close();
