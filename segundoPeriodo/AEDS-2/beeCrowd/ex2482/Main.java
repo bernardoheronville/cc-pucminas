65aebde92a530889a87d7eaf03a54dcc95314212
bernardoheronville <bernardoheronville@gmail.com>
2025-09-10 12:13:42 -0300

    Iniciado o treinamento para prova pratica beecrowed ex 2482

---
+++ segundoPeriodo/AEDS-2/beeCrowd/ex2482/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            sc.nextLine();
+            String arrayIdioma[] = new String[n];
+            String arrayFeliz[] = new String[n];
+            for(int i = 0; i < n; i++) {
+                arrayIdioma[i] = sc.nextLine();
+                arrayFeliz[i] = sc.nextLine();
+            }
+            int M = sc.nextInt();
+            sc.nextLine();
+            String arrayNomes[] = new String[M];
+            String arrayLingua[] = new String[M];
+            for(int i = 0; i < M; i++) {
+                arrayNomes[i] = sc.nextLine();
+                arrayLingua[i] = sc.nextLine();
+            }
+            for(int i = 0; i < M; i++) {
+                System.out.println(arrayNomes[i]);
+                for(int j = 0; j < n; j++) {
+                    if(arrayLingua[i].equals(arrayIdioma[j])) {
+                        System.out.println(arrayFeliz[j]);
+                    }
+                }
+            }
+        } 
+    }
+}
