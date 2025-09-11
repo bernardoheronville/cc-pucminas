32a2bf36cefc7394b3105f157348106d4f58bf8d
bernardoheronville <bernardoheronville@gmail.com>
2025-09-11 16:31:29 -0300

    Exercicio 3176 do beecrowed finalizado

--- segundoPeriodo/AEDS-2/beeCrowd/ex3176/main.c
+++ segundoPeriodo/AEDS-2/beeCrowd/ex3176/main.c
@@ -5,16 +5,15 @@ typedef struct {
     int idade;
 } Duendes;
 
-void ordenar(Duendes duendes[], int n) {
-    for (int i = 0; i < (n - 1); i++) {
-        int idadeMaior = duendes[i].idade, aux = i;
-        for(int j = i + 1; j < n; j++) {
-            if(idadeMaior > duendes[j].idade) {
-                idadeMaior = duendes[j].idade;
-                aux = j;
-            }
+void ordenar(Duendes duendes[] ,int n) {
+    for(int i = 0; i < n; i++) {
+        Duendes tmp = duendes[i];
+        int j = i - 1;
+        while(j >= 0 && duendes[j].idade < tmp.idade) {
+            duendes[j + 1] = duendes[j];
+            j--;
         }
-        printf("%d\n",duendes[aux].idade);
+        duendes[j + 1] = tmp;
     }
 }
 
@@ -22,9 +21,17 @@ int main() {
     int n;
     scanf("%d",&n);
     Duendes duendes[n];
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         scanf("%s %d", duendes[i].nome, &duendes[i].idade);
     }
     ordenar(duendes, n);
+    int numTimes = n/3;
+    for (int i = 0; i < numTimes; i++) {
+        printf("Time %d\n", i + 1);
+        printf("%s %d\n", duendes[i].nome, duendes[i].idade);                      
+        printf("%s %d\n", duendes[i + numTimes].nome, duendes[i + numTimes].idade); 
+        printf("%s %d\n", duendes[i + 2 * numTimes].nome, duendes[i + 2 * numTimes].idade);
+        printf("\n");
+    }
     return 0;
 }
