e5543d2fd3c51fdb93d71a5b11e4359434a57d84
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-09-13 10:50:29 -0300

    Tp 03 ex 02 resolvido

--- segundoPeriodo/AEDS-2/tps/tp03/ex02/Main.java
+++ segundoPeriodo/AEDS-2/tps/tp03/ex02/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-// Funcao para retornar true or false se as string sao iguais.
+	// Funcao para retornar true or false se as string sao iguais.
 	public static boolean my_strcmp(String palavra1, String palavra2) {
 		boolean resp = true;
 		if(palavra1.length() == palavra2.length()) {
@@ -18,29 +18,27 @@ public class Main {
 		return resp;
 	}
 
-    //Funcao recursiva que recebe uma string como parâmetro e retorna a string invertida.
-    public static String inverteString(String palavra, String aux, int i) {
-        if(i < 0) return aux;
-        else {
-            aux += palavra.charAt(i);
-            return inverteString(palavra, aux, i - 1);
-        }
+    // Funcao recursiva que recebe uma string como parâmetro e retorna a string invertida.
+    public static String inverteString(String palavra, String resp, int i) {
+		char caracter = palavra.charAt(i);
+        if(i == 0) resp += caracter;
+        else resp += (caracter + inverteString(palavra, resp, i - 1));
+		return resp;
     }
 
-    //Funcao auxiliar de inverte String.
-    public static String inverteAux(String palavra) {
-        int tam = palavra.length() - 1;
-        String aux = "";
-        return inverteString(palavra, aux, tam);
+    // Funcao auxiliar de inverte String.
+    public static String auxInverteString(String palavra) {
+        String resp = "";
+        return inverteString(palavra, resp, palavra.length() - 1);
     }
 
-    //Main
+    // Main
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 		String palavra = new String();
 		palavra = sc.nextLine();
 		while(!my_strcmp(palavra, "FIM")) {
-			System.out.println(inverteAux(palavra));
+			System.out.println(auxInverteString(palavra));
 			palavra = sc.nextLine();
 		}
 		sc.close();
