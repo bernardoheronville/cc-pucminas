94cf1d332717975686068c79991c6d0b0fda91a1
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-09-25 19:27:04 -0300

    Lab 05 finalizado

--- segundoPeriodo/AEDS-2/labs/lab05/ex01/Main.java
+++ segundoPeriodo/AEDS-2/labs/lab05/ex01/Main.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Random;
 
 public class Main {
 
-    // ---------- QUICKSORTS ----------
     public static void QuickSortFirstPivot(int array[], int left, int right) {
+        if (left >= right) return;
         int i = left, j = right;
         int pivo = array[left];
         while(i <= j) {
@@ -15,11 +15,12 @@ public class Main {
                 j--;
             }
         }
-        if(left < j) QuickSortFirstPivot(array, left, j);
-        if(i < right) QuickSortFirstPivot(array, i, right);
+        QuickSortFirstPivot(array, left, j);
+        QuickSortFirstPivot(array, i, right);
     }
 
     public static void QuickSortLastPivot(int array[], int left, int right) {
+        if (left >= right) return;
         int i = left, j = right;
         int pivo = array[right];
         while(i <= j) {
@@ -31,11 +32,12 @@ public class Main {
                 j--;
             }
         }
-        if(left < j) QuickSortLastPivot(array, left, j);
-        if(i < right) QuickSortLastPivot(array, i, right);
+        QuickSortLastPivot(array, left, j);
+        QuickSortLastPivot(array, i, right);
     }
 
     public static void QuickSortRandomPivot(int array[], int left, int right) {
+        if (left >= right) return;
         int i = left, j = right;
         Random gerador = new Random();
         int pivoIndex = left + gerador.nextInt(right - left + 1);
@@ -49,17 +51,25 @@ public class Main {
                 j--;
             }
         }
-        if(left < j) QuickSortRandomPivot(array, left, j);
-        if(i < right) QuickSortRandomPivot(array, i, right);
+        QuickSortRandomPivot(array, left, j);
+        QuickSortRandomPivot(array, i, right);
     }
 
     public static void QuickSortMedianOfThree(int array[], int left, int right) {
+        if (left >= right) return;
         int i = left, j = right;
-        int a = array[left], b = array[(left + right) / 2], c = array[right];
+        int mid = (left + right) / 2;
+        int a = array[left], b = array[mid], c = array[right];
         int pivo;
-        if ((a >= b && a <= c) || (a <= b && a >= c)) pivo = a;
-        else if ((b >= a && b <= c) || (b <= a && b >= c)) pivo = b;
-        else pivo = c;
+
+        // Lógica corrigida para encontrar a mediana corretamente
+        if ((a >= b && a <= c) || (a <= b && a >= c)) {
+            pivo = a;
+        } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+            pivo = b;
+        } else {
+            pivo = c;
+        }
 
         while(i <= j) {
             while(array[i] < pivo) i++;
@@ -70,11 +80,10 @@ public class Main {
                 j--;
             }
         }
-        if(left < j) QuickSortMedianOfThree(array, left, j);
-        if(i < right) QuickSortMedianOfThree(array, i, right);
+        QuickSortMedianOfThree(array, left, j);
+        QuickSortMedianOfThree(array, i, right);
     }
 
-    // ---------- UTILITÁRIOS ----------
     public static void swap(int array[], int i, int j) {
         int temp = array[i];
         array[i] = array[j];
@@ -98,8 +107,9 @@ public class Main {
     public static void meioOrdenado(int array[], int n) {
         Random rand = new Random();
         ordenado(array, n);
-        for (int i = 0; i < n / 2; i++) {
-            swap(array, i, rand.nextInt(n));
+        // Desordena 25% do array para um resultado "meio ordenado" mais realista
+        for (int i = 0; i < n / 4; i++) {
+            swap(array, rand.nextInt(n), rand.nextInt(n));
         }
     }
 
@@ -107,62 +117,60 @@ public class Main {
         return System.nanoTime();
     }
 
-    // ---------- MAIN ----------
     public static void main(String args[]) {
-        // Tamanhos que queremos testar
-        int[] tamanhos = {100, 1000, 10000};
+        int tamanhos[] = {100, 1000, 10000};
+        String nomesCenarios[] = {"Ordenado", "Meio Ordenado", "Aleatório"};
 
         for (int n : tamanhos) {
-            
-            int[] arrayOrdenado = new int[n];
-            ordenado(arrayOrdenado, n);
-
-            int[] arrayMeioOrdenado = new int[n];
-            meioOrdenado(arrayMeioOrdenado, n);
-
-            int[] arrayAleatorio = new int[n];
-            random(arrayAleatorio, n);
-            
-            int[][] cenarios = {arrayOrdenado, arrayMeioOrdenado, arrayAleatorio};
-            String[] nomesCenarios = {"Array Ordenado", "Array Meio Ordenado", "Array Aleatório"};
-
             System.out.println("\n#####################################################");
             System.out.println("   INICIANDO TESTES PARA N = " + n);
             System.out.println("#####################################################");
 
-            for (int i = 0; i < cenarios.length; i++) {
+            for (String cenario : nomesCenarios) {
                 System.out.println("-----------------------------------------------------");
-                System.out.println("CENÁRIO: " + nomesCenarios[i]);
+                System.out.println("CENÁRIO: " + cenario);
                 System.out.println("-----------------------------------------------------");
 
-                int[] original = cenarios[i];
                 long inicio, fim;
+                int arrayTeste[];
 
-                // Pivô Primeiro
-                int[] copia1 = Arrays.copyOf(original, n);
+                arrayTeste = new int[n];
+                if (cenario.equals("Ordenado")) ordenado(arrayTeste, n);
+                else if (cenario.equals("Meio Ordenado")) meioOrdenado(arrayTeste, n);
+                else random(arrayTeste, n);
+                
                 inicio = now();
-                QuickSortFirstPivot(copia1, 0, n - 1);
+                QuickSortFirstPivot(arrayTeste, 0, n - 1);
                 fim = now();
-                System.out.printf("Pivô Primeiro:   %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+                System.out.printf("Pivô Primeiro: %.3f ms.\n", (fim - inicio) / 1_000_000.0);
 
-                // Pivô Último
-                int[] copia2 = Arrays.copyOf(original, n);
+                arrayTeste = new int[n]; 
+                if (cenario.equals("Ordenado")) ordenado(arrayTeste, n);
+                else if (cenario.equals("Meio Ordenado")) meioOrdenado(arrayTeste, n);
+                else random(arrayTeste, n);
+                
                 inicio = now();
-                QuickSortLastPivot(copia2, 0, n - 1);
+                QuickSortLastPivot(arrayTeste, 0, n - 1);
                 fim = now();
-                System.out.printf("Pivô Último:     %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+                System.out.printf("Pivô Último: %.3f ms.\n", (fim - inicio) / 1_000_000.0);
 
-                // Pivô Aleatório
-                int[] copia3 = Arrays.copyOf(original, n);
+                arrayTeste = new int[n]; 
+                if (cenario.equals("Ordenado")) ordenado(arrayTeste, n);
+                else if (cenario.equals("Meio Ordenado")) meioOrdenado(arrayTeste, n);
+                else random(arrayTeste, n);
+
                 inicio = now();
-                QuickSortRandomPivot(copia3, 0, n - 1);
+                QuickSortRandomPivot(arrayTeste, 0, n - 1);
                 fim = now();
                 System.out.printf("Pivô Aleatório:  %.3f ms.\n", (fim - inicio) / 1_000_000.0);
 
-                // Mediana de Três
-                int[] copia4 = Arrays.copyOf(original, n);
+                arrayTeste = new int[n]; 
+                if (cenario.equals("Ordenado")) ordenado(arrayTeste, n);
+                else if (cenario.equals("Meio Ordenado")) meioOrdenado(arrayTeste, n);
+                else random(arrayTeste, n);
+
                 inicio = now();
-                QuickSortMedianOfThree(copia4, 0, n - 1);
+                QuickSortMedianOfThree(arrayTeste, 0, n - 1);
                 fim = now();
                 System.out.printf("Mediana de Três: %.3f ms.\n", (fim - inicio) / 1_000_000.0);
             }
