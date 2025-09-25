b824c73b4705efc0d56a0b2789688c7861c153db
bernardoheronville <bernardoheronville@gmail.com>
2025-09-25 16:21:11 -0300

    .

--- segundoPeriodo/AEDS-2/labs/lab05/ex01/Main.java
+++ segundoPeriodo/AEDS-2/labs/lab05/ex01/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Main {
 
+    // ---------- QUICKSORTS ----------
     public static void QuickSortFirstPivot(int array[], int left, int right) {
         int i = left, j = right;
         int pivo = array[left];
         while(i <= j) {
             while(array[i] < pivo) i++;
             while(array[j] > pivo) j--;
-            if(i <=j) {
+            if(i <= j) {
                 swap(array, i, j);
                 i++;
                 j--;
@@ -24,7 +25,7 @@ public class Main {
         while(i <= j) {
             while(array[i] < pivo) i++;
             while(array[j] > pivo) j--;
-            if(i <=j) {
+            if(i <= j) {
                 swap(array, i, j);
                 i++;
                 j--;
@@ -42,7 +43,7 @@ public class Main {
         while(i <= j) {
             while(array[i] < pivo) i++;
             while(array[j] > pivo) j--;
-            if(i <=j) {
+            if(i <= j) {
                 swap(array, i, j);
                 i++;
                 j--;
@@ -56,13 +57,14 @@ public class Main {
         int i = left, j = right;
         int a = array[left], b = array[(left + right) / 2], c = array[right];
         int pivo;
-        if (a > b && a < c) pivo = a;
-        else if (b > a && b < c) pivo = b;
+        if ((a >= b && a <= c) || (a <= b && a >= c)) pivo = a;
+        else if ((b >= a && b <= c) || (b <= a && b >= c)) pivo = b;
         else pivo = c;
+
         while(i <= j) {
             while(array[i] < pivo) i++;
             while(array[j] > pivo) j--;
-            if(i <=j) {
+            if(i <= j) {
                 swap(array, i, j);
                 i++;
                 j--;
@@ -72,6 +74,7 @@ public class Main {
         if(i < right) QuickSortMedianOfThree(array, i, right);
     }
 
+    // ---------- UTILITÁRIOS ----------
     public static void swap(int array[], int i, int j) {
         int temp = array[i];
         array[i] = array[j];
@@ -79,48 +82,90 @@ public class Main {
     }
 
     public static void ordenado(int array[], int n) {
-		for (int i = 0; i < n; i++) {
-			array[i] = i;
-		}
-	}
+        for (int i = 0; i < n; i++) {
+            array[i] = i;
+        }
+    }
 
     public static void random(int array[], int n) {
-		Random rand = new Random();
-		ordenado(array, n);	
-		for (int i = 0; i < n; i++) {
-			swap(array, i, Math.abs(rand.nextInt()) % n);
-		}
-	}
+        Random rand = new Random();
+        ordenado(array, n);
+        for (int i = 0; i < n; i++) {
+            swap(array, i, rand.nextInt(n));
+        }
+    }
 
     public static void meioOrdenado(int array[], int n) {
-		Random rand = new Random();
-		ordenado(array, n);	
-		for (int i = 0; i < n / 2; i++) {
-			swap(array, i, Math.abs(rand.nextInt()) % n);
-		}
-	}
+        Random rand = new Random();
+        ordenado(array, n);
+        for (int i = 0; i < n / 2; i++) {
+            swap(array, i, rand.nextInt(n));
+        }
+    }
 
     public static long now() {
         return System.nanoTime();
     }
 
+    // ---------- MAIN ----------
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+        // Tamanhos que queremos testar
+        int[] tamanhos = {100, 1000, 10000};
 
-        int n = 100;
-        int array[] = new int[n];
+        for (int n : tamanhos) {
+            
+            int[] arrayOrdenado = new int[n];
+            ordenado(arrayOrdenado, n);
 
-        //ordenado(array, n);
-        meioOrdenado(array, n);
-        //random(array, n);
-        
-        long inicio = now();
-        //QuickSortFirstPivot(array, 0, n - 1);
-        //QuickSortLastPivot(array, 0, n - 1);
-        //QuickSortRandomPivot(array, 0, n - 1);
-        QuickSortMedianOfThree(array, 0, n - 1);
-        long fim = now();
+            int[] arrayMeioOrdenado = new int[n];
+            meioOrdenado(arrayMeioOrdenado, n);
 
-        System.out.println("Tempo para ordenar: " + (fim - inicio) / 1_000_000.0 + " ms.");
+            int[] arrayAleatorio = new int[n];
+            random(arrayAleatorio, n);
+            
+            int[][] cenarios = {arrayOrdenado, arrayMeioOrdenado, arrayAleatorio};
+            String[] nomesCenarios = {"Array Ordenado", "Array Meio Ordenado", "Array Aleatório"};
+
+            System.out.println("\n#####################################################");
+            System.out.println("   INICIANDO TESTES PARA N = " + n);
+            System.out.println("#####################################################");
+
+            for (int i = 0; i < cenarios.length; i++) {
+                System.out.println("-----------------------------------------------------");
+                System.out.println("CENÁRIO: " + nomesCenarios[i]);
+                System.out.println("-----------------------------------------------------");
+
+                int[] original = cenarios[i];
+                long inicio, fim;
+
+                // Pivô Primeiro
+                int[] copia1 = Arrays.copyOf(original, n);
+                inicio = now();
+                QuickSortFirstPivot(copia1, 0, n - 1);
+                fim = now();
+                System.out.printf("Pivô Primeiro:   %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+
+                // Pivô Último
+                int[] copia2 = Arrays.copyOf(original, n);
+                inicio = now();
+                QuickSortLastPivot(copia2, 0, n - 1);
+                fim = now();
+                System.out.printf("Pivô Último:     %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+
+                // Pivô Aleatório
+                int[] copia3 = Arrays.copyOf(original, n);
+                inicio = now();
+                QuickSortRandomPivot(copia3, 0, n - 1);
+                fim = now();
+                System.out.printf("Pivô Aleatório:  %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+
+                // Mediana de Três
+                int[] copia4 = Arrays.copyOf(original, n);
+                inicio = now();
+                QuickSortMedianOfThree(copia4, 0, n - 1);
+                fim = now();
+                System.out.printf("Mediana de Três: %.3f ms.\n", (fim - inicio) / 1_000_000.0);
+            }
+        }
     }
 }
