4119ce708990af00263bc189bf853c8ba72472f0
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-10-05 21:47:09 -0300

    tp 04 quase finalizado

--- segundoPeriodo/AEDS-2/tps/tp04/ex01/Main.java
+++ segundoPeriodo/AEDS-2/tps/tp04/ex01/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 
+// Classe Game.
 class Game {
     private int id;
     private String nome;
@@ -17,32 +18,55 @@ class Game {
     private String generos[];
     private String tags[];
 
+    // Construtor da classe Game.
+    public Game() {
+        this.id = 0;
+        this.nome = "";
+        this.data = "01/01/0000";
+        this.jogadores = 0;
+        this.preco = 0.0f;
+        this.idiomas = new String[0];
+        this.notaEspecial = -1;
+        this.notaUsuario = -1.0f;
+        this.conquistas = 0;
+        this.empresasPublicacao = new String[0];
+        this.empresasEstudios = new String[0];
+        this.categorias = new String[0];
+        this.generos = new String[0];
+        this.tags = new String[0];
+    }
+
+    // Metodos setters.
     public void setID(String id) {
-        this.id = Integer.parseInt(id);
+        if (id != null && !id.isEmpty()) {
+            this.id = Integer.parseInt(id);
+        }
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
     public void setData(String data) {
-        String mes = "", dia = "", ano = "", resp = "";
-        for(int i = 0; i < 3; i++) {
+        if (data == null || data.isEmpty()) {
+            this.data = "01/01/0000";
+            return;
+        }
+        String mes = "", dia = "", ano = "";
+        for (int i = 0; i < 3; i++) {
             mes += data.charAt(i);
         }
-        if(data.charAt(5) == ',') {
-            dia += "0";
-            dia += data.charAt(4);
-            for(int i = 7; i < data.length(); i++) {
+        if (data.charAt(5) == ',') {
+            dia = "0" + data.charAt(4);
+            for (int i = 7; i < data.length(); i++) {
                 ano += data.charAt(i);
             }
-        }
-        else {
+        } else {
             dia += data.charAt(4);
             dia += data.charAt(5);
-            for(int i = 8; i < data.length(); i++) {
+            for (int i = 8; i < data.length(); i++) {
                 ano += data.charAt(i);
             }
         }
-        switch(mes) {
+        switch (mes) {
             case "Jan": mes = "01"; break;
             case "Feb": mes = "02"; break;
             case "Mar": mes = "03"; break;
@@ -55,10 +79,9 @@ class Game {
             case "Oct": mes = "10"; break;
             case "Nov": mes = "11"; break;
             case "Dec": mes = "12"; break;
-            default: mes = "01"; break;     
+            default: mes = "01"; break;
         }
-        resp = dia + "/" + mes + "/" + ano;
-        this.data = resp;
+        this.data = dia + "/" + mes + "/" + ano;
     }
     public void setJogadores(String jogadores) {
         String aux = "";
@@ -70,7 +93,7 @@ class Game {
         this.jogadores = Integer.parseInt(aux);
     }
     public void setPreco(String preco) {
-        if(preco.equals("Free to play")) {
+        if(preco.equals("Free to Play")) {
             this.preco = 0.0f;
         }
         else {
@@ -81,26 +104,23 @@ class Game {
         this.idiomas = formatar(idiomas);
     }
     public void setNotaEspecial(String notaEspecial) {
-        if(notaEspecial == null) {
-            this.notaEspecial = -1;
-        }
-        else {
+        if(notaEspecial == null || notaEspecial.isEmpty()) {
+            this.notaEspecial = -1; 
+        } else {
             this.notaEspecial = Integer.parseInt(notaEspecial);
         }
     }
     public void setNotaUsuario(String notaUsuario) {
-        if(notaUsuario == null || notaUsuario == "tbd") {
-            this.notaUsuario = -1.0f;
-        }
-        else {
+        if(notaUsuario == null || notaUsuario.isEmpty() || notaUsuario.equals("tbd")) {
+            this.notaUsuario = -1.0f; 
+        } else {
             this.notaUsuario = Float.parseFloat(notaUsuario);
         }
     }
     public void setConquistas(String conquistas) {
-        if(conquistas == null) {
+        if(conquistas == null || conquistas.isEmpty()) {
             this.conquistas = 0;
-        }
-        else {
+        } else {
             this.conquistas = Integer.parseInt(conquistas);
         } 
     }
@@ -119,62 +139,106 @@ class Game {
     public void setTags(String tags) {
        this.tags = formatar(tags);
     }
+
+    // Metodos getters.
+    public int getID() { return id; };
+    public String getNome() { return nome; };
+    public String getData() { return data; };
+    public int getJogadores() { return jogadores; };
+    public float getPreco() { return preco; }
+    public String[] getIdiomas() { return idiomas; };
+    public int getNotaEspecial() { return notaEspecial; };
+    public float getNotaUsuario() { return notaUsuario; }
+    public int getConquistas() { return conquistas; };
+    public String[] getEmpresasPublicacao() { return empresasPublicacao; };
+    public String[] getEmpresasEstudios() { return empresasEstudios; };
+    public String[] getCategorias() { return categorias; };
+    public String[] getGeneros() { return generos; };
+    public String[] getTags() { return tags; };
+
+    // Funcao que formata as strings.
     public String[] formatar(String entrada) {
-        if (entrada == null) {
-            return new String[0]; 
+        if (entrada == null || entrada.isEmpty() || entrada.equals("[]")) {
+            return new String[0];
         }
-        int virgulas = 0;
+        int virgulas = 1;
         for(int i = 0; i < entrada.length(); i++) {
             char c = entrada.charAt(i);
             if(c == ',') {
                 virgulas++;
             }
         }
-        String resp[] = new String[virgulas + 1];
+        String resp[] = new String[virgulas];
         String aux = "";
         int contador = 0;
         for(int i = 0; i < entrada.length(); i++) {
-            char c = entrada.charAt(i); 
+            char c = entrada.charAt(i);
             if(c == ',') {
-                resp[contador] = aux;
+                resp[contador] = limparString(aux);
                 contador++;
                 aux = "";
-            }
-            else {
-                if(!(c == '[' || c == ']' || c == '\'')) {
+            } else {
+                if(!(c == '[' || c == ']')) {
                     aux += c;
-                } 
+                }
             }
         }
-        resp[contador] = aux;
+        resp[contador] = limparString(aux);
         return resp;
     }
-    public int getID() { return id; };
+    // Funcao aulixiar para formatar os espacos.
+    private String limparString(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+        int inicio = 0;
+        int fim = str.length() - 1;
 
-    private String auxiliarMostrar(String array[], int aux) {
+        while (inicio < str.length() && str.charAt(inicio) <= ' ') {
+            inicio++;
+        }
+
+        while (fim >= inicio && str.charAt(fim) <= ' ') {
+            fim--;
+        }
+        if (inicio > fim) {
+            return "";
+        }
+        if ((fim - inicio + 1) >= 2 && str.charAt(inicio) == '\'' && str.charAt(fim) == '\'') {
+            inicio++;
+            fim--;
+        }
+        String resultado = "";
+        for (int i = inicio; i <= fim; i++) {
+            resultado += str.charAt(i);
+        }
+        return resultado;
+    }
+
+    // Funcao para mostrar os atributos da classe.
+    private String auxiliarMostrar(String array[]) {
         String result = "[";
-        for(int i = 0; i < array.length; i++) {
-            result += array[i];
-            if(i < array.length - 1 && aux == 0) {
-                result += ",";
-            }
-            else if(i < array.length - 1 && aux == 1) {
-                result += ", ";
+        if (array != null) {
+            for(int i = 0; i < array.length; i++) {
+                result += array[i];
+                if(i < array.length - 1) {
+                    result += ", ";
+                }
             }
         }
         result += "]";
         return result;
     }
-
     @Override 
     public String toString() {
-        return ("=> " + id + " ## " + nome + " ## " + data + " ## " + jogadores + " ## " + preco + " ## " + auxiliarMostrar(idiomas, 0) + " ## " + notaEspecial
-                + " ## " + notaUsuario + " ## " + conquistas + " ## " + auxiliarMostrar(empresasPublicacao, 1) + " ## " + auxiliarMostrar(empresasEstudios, 1) + " ## " + auxiliarMostrar(categorias, 1)
-                + " ## " + auxiliarMostrar(generos, 1) + " ## " + auxiliarMostrar(tags, 1) + " ## ");
+        return ("=> " + id + " ## " + nome + " ## " + data + " ## " + jogadores + " ## " + preco + " ## " + auxiliarMostrar(idiomas) + " ## " + notaEspecial
+                + " ## " + notaUsuario + " ## " + conquistas + " ## " + auxiliarMostrar(empresasPublicacao) + " ## " + auxiliarMostrar(empresasEstudios) + " ## " + auxiliarMostrar(categorias)
+                + " ## " + auxiliarMostrar(generos) + " ## " + auxiliarMostrar(tags) + " ##");
     }
 }
 
 public class Main {
+    // Procedimento auxiliar do metodo settar.
     public static void settar(Game game, String array[]) {
         game.setID(array[0]);
         game.setNome(array[1]);
@@ -192,12 +256,14 @@ public class Main {
         game.setTags(array[13]);
     }
 
+    // Main
     public static void main(String args[]) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         File arq = new File("pubs/games.csv");
         Scanner scfile = new Scanner(arq);
         Game game[] = new Game[2000];
         int jogos = 0;
+        scfile.nextLine();
         while(scfile.hasNextLine()) { 
             String entrada = scfile.nextLine();
             String array[] = new String[14]; 
--- segundoPeriodo/AEDS-2/tps/tp04/ex02/main.c
+++ segundoPeriodo/AEDS-2/tps/tp04/ex02/main.c
@@ -3,136 +3,246 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct  
-{
+// Struct String para auxilio.
+typedef struct {
+    char str[1000];
+} String;
+
+// Struct Game.
+typedef struct {
     int id;
-    char *nome;
-    char *data;
+    String nome;
+    String data;
     int jogadores;
     float preco;
-    char **idiomas;
+    String idiomas[50];
+    int num_idiomas; 
     int notaEspecial;
     float notaUsuario;
     int conquistas;
-    char **empresasPublicacao;
-    char **empresasEstudios;
-    char **categorias;
-    char **generos;
-    char **tags;
+    String empresasPublicacao[50];
+    int num_empresasPublicacao;
+    String empresasEstudios[50];
+    int num_empresasEstudios;
+    String categorias[50];
+    int num_categorias;
+    String generos[50];
+    int num_generos;
+    String tags[50];
+    int num_tags;
 } Game;
 
-int my_strlen(char *str) {
-    int count = 0;
-    while(*(str + count) != '\0') {
-        count++;
-    }
-    return count;
-}
-
-bool my_strcmp(char *entrada1, char *entrada2) {
-    bool resp = true;
-    if(my_strlen(entrada1) == my_strlen(entrada2)) {
-        for(int i = 0; i < my_strlen(entrada1); i++) {
-            if(*(entrada1 + i) != *(entrada2 + i)) {
-                resp = false;
-                i = my_strlen(entrada1);
+// Funcao que formata uma string separando uma lista como por exemplo ("['a', 'b', 'c']") em um array de strings.
+int formatar(String entrada, String saida[], bool apostrofo) {
+    String aux;
+    int contador = 0, auxPos = 0;
+    for(int i = 0; i < strlen(entrada.str); i++) {
+        char c = entrada.str[i]; 
+        if(c == ',') {
+            aux.str[auxPos] = '\0';
+            int start = 0;
+            while(aux.str[start] == ' ') start++;
+            if (strlen(aux.str + start) > 0) {
+                 strcpy(saida[contador].str, aux.str + start);
+                 contador++;
             }
+            strcpy(aux.str, ""); 
+            auxPos = 0;
         }
-    }
-    else {
-        resp = false;
-    }
-    return resp;
-}
-
-void settar(Game *game, char **array) {
-    if(*(array + 0) != NULL) game->id = atoi(*(array + 0));
-    if(*(array + 1) != NULL) game->nome = strdup(*(array + 1));
-    if(*(array + 2) != NULL) game->data = strdup(*(array + 2));
-    if(*(array + 3) != NULL) game->jogadores = atoi(*(array + 3));
-    if(*(array + 4) != NULL) game->preco = atof(*(array + 4));
-    if(*(array + 5) != NULL) game->idiomas = strdup(*(array + 5));
-    if(*(array + 6) != NULL) game->notaEspecial = atoi(*(array + 6));
-    if(*(array + 7) != NULL) game->notaUsuario = atof(*(array + 7));
-    if(*(array + 8) != NULL) game->conquistas = atoi(*(array + 8));
-    if(*(array + 9) != NULL) game->empresasPublicacao = strdup(*(array + 9));
-    if(*(array + 10) != NULL) game->empresasEstudios = strdup(*(array + 10));
-    if(*(array + 11) != NULL) game->categorias = strdup(*(array + 11));
-    if(*(array + 12) != NULL) game->generos = strdup(*(array + 12));
-    if(*(array + 13) != NULL) game->tags = strdup(*(array + 13));
-}
-
-void imprimir(Game *game) {
-    printf("=> %d ## %s ## %s ## %d ## %.2f ## [%s] ## %d ## %.2f ## %d ## [%s] ## [%s] ## [%s] ## [%s] ## [%s] ##",
-    game->id, game->nome, game->data, game->jogadores, game->preco, game->idiomas, game->notaEspecial, game->notaUsuario,
-    game->conquistas, game->empresasPublicacao, game->empresasEstudios, game->categorias, game->generos, game->tags
-    );
-}
-
-int main() {
-    FILE *arq = fopen("games.csv","r");
-    if (arq == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        return 1;
-    }
-    int jogos = 0;
-    Game *game = (Game*)malloc(2000*sizeof(Game));
-    char *entrada = (char*)malloc(1000*sizeof(char));
-    while(fscanf(arq," %[^\n]",entrada) != EOF) {
-        char **array = (char**)malloc(14*sizeof(char*));
-        char *aux = (char)calloc(1000, sizeof(char));
-        int contador = 0, auxPos = 0;
-        bool aspas = false;
-        for(int i = 0; i < my_strlen(entrada); i++) {
-            char c = *(entrada + i);
-            if(c == '"') {
-                aspas = !aspas;
-            }
-            else if(c == ',' && !aspas) {
-                *(aux + auxPos) = '\0';
-                *(array + contador) = strdup(aux);
-                contador++;
-                auxPos = 0;
-                strcpy(aux, "");
-            }
-            else {
-                *(aux + auxPos) = c;
+        else {
+            if(!(c == '[' || c == ']' || (apostrofo && c == '\''))) {
+                aux.str[auxPos] = c;
                 auxPos++;
             }
         }
-        *(aux + auxPos) = '\0';
-        *(array + contador) = strdup(aux);
-        settar((game + jogos), array);
-        jogos++;
-        for(int i = 0 ; i < 14; i++) {
-            free((array + i));
+    }
+    aux.str[auxPos] = '\0';
+    if (strlen(aux.str) > 0) {
+        int start = 0;
+        while(aux.str[start] == ' ') start++;
+        strcpy(saida[contador].str, aux.str + start);
+        contador++;
+    }
+    return contador;
+}
+// Procedimento que transforma "Oct 18, 2018" em 18/10/2018.
+void setData(String entrada, String *saida) {
+    if (strlen(entrada.str) < 8) { 
+        strcpy(saida->str, "01/01/0000");
+        return;
+    }
+    char mes[4], dia[3], ano[5], mesNum[3];
+    strncpy(mes, entrada.str, 3);
+    mes[3] = '\0';
+    if (entrada.str[5] == ',') {
+        dia[0] = '0';
+        dia[1] = entrada.str[4];
+        dia[2] = '\0';
+        strcpy(ano, entrada.str + 7);
+    } else { 
+        dia[0] = entrada.str[4];
+        dia[1] = entrada.str[5];
+        dia[2] = '\0';
+        strcpy(ano, entrada.str + 8);
+    }
+    if (strcmp(mes, "Jan") == 0) strcpy(mesNum, "01");
+    else if (strcmp(mes, "Feb") == 0) strcpy(mesNum, "02");
+    else if (strcmp(mes, "Mar") == 0) strcpy(mesNum, "03");
+    else if (strcmp(mes, "Apr") == 0) strcpy(mesNum, "04");
+    else if (strcmp(mes, "May") == 0) strcpy(mesNum, "05");
+    else if (strcmp(mes, "Jun") == 0) strcpy(mesNum, "06");
+    else if (strcmp(mes, "Jul") == 0) strcpy(mesNum, "07");
+    else if (strcmp(mes, "Aug") == 0) strcpy(mesNum, "08");
+    else if (strcmp(mes, "Sep") == 0) strcpy(mesNum, "09");
+    else if (strcmp(mes, "Oct") == 0) strcpy(mesNum, "10");
+    else if (strcmp(mes, "Nov") == 0) strcpy(mesNum, "11");
+    else if (strcmp(mes, "Dec") == 0) strcpy(mesNum, "12");
+    else strcpy(mesNum, "01");
+    strcpy(saida->str, dia);
+    strcat(saida->str, "/");
+    strcat(saida->str, mesNum);
+    strcat(saida->str, "/");
+    strcat(saida->str, ano);
+}
+
+// Procedimento que setta cada atributo da struct.
+void settar(Game *game, String array[]) {
+    game->id = atoi(array[0].str);
+    strcpy(game->nome.str, array[1].str);
+    setData(array[2], &game->data);
+    String aux;
+    aux.str[0] = '\0';
+    int AuxPos = 0;
+    for(int i = 0; i < strlen(array[3].str); i++) {
+        char c = array[3].str[i];
+        if(c >= '0' && c <= '9') {
+            aux.str[AuxPos++] = c;
         }
-        free(aux);
+    }
+    aux.str[AuxPos] = '\0';
+    game->jogadores = atoi(aux.str);
+    if(strcmp(array[4].str, "Free to Play") == 0 || strcmp(array[4].str, "0.0") == 0) {
+        game->preco = 0.0f;
+    } 
+    else {
+        game->preco = atof(array[4].str); 
+    }
+    if(strlen(array[6].str) == 0) {
+        game->notaEspecial = 0;
+    } 
+    else {
+        game->notaEspecial = atoi(array[6].str);
+    }
+    if(strlen(array[7].str) == 0 || strcmp(array[7].str, "tbd") == 0) {
+        game->notaUsuario = 0.0f;
+    } 
+    else {
+        game->notaUsuario = atof(array[7].str);
+    }
+    if(strlen(array[8].str) == 0) {
+        game->conquistas = 0;
+    } 
+    else {
+        game->conquistas = atoi(array[8].str);
+    }
+    game->num_idiomas = formatar(array[5], game->idiomas, true); 
+    game->num_empresasPublicacao = formatar(array[9], game->empresasPublicacao, false);
+    game->num_empresasEstudios = formatar(array[10], game->empresasEstudios, false);
+    game->num_categorias = formatar(array[11], game->categorias, false);
+    game->num_generos = formatar(array[12], game->generos, false);
+    game->num_tags = formatar(array[13], game->tags, false);
+}
+
+// Procedimento que imprimi o array a partir de seu tamanho.
+void imprimirArray(String arr[], int n) {
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        int start = 0;
+        while(arr[i].str[start] == ' ') start++;
+        printf("%s", arr[i].str + start);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("]");
+}
+
+// Procedimento de imprimir normal
+void imprimir(Game *game) {
+    printf("=> %d ## %s ## %s ## %d ## %.2f ",
+        game->id, 
+        game->nome.str, 
+        game->data.str,
+        game->jogadores,
+        game->preco
+    );
+    imprimirArray(game->idiomas, game->num_idiomas);
+    printf(" ## %d ## %.1f ## %d ## ", 
+        game->notaEspecial, 
+        game->notaUsuario,
+        game->conquistas
+    );
+    imprimirArray(game->empresasPublicacao, game->num_empresasPublicacao);
+    printf(" ## ");
+    imprimirArray(game->empresasEstudios, game->num_empresasEstudios);
+    printf(" ## ");
+    imprimirArray(game->categorias, game->num_categorias);
+    printf(" ## ");
+    imprimirArray(game->generos, game->num_generos);
+    printf(" ## ");
+    imprimirArray(game->tags, game->num_tags);
+    printf(" ##\n");
+}
+
+// Main 
+int main() {
+    FILE *arq = fopen("pubs/games.csv", "r");
+    if (!arq) {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    Game *game = (Game*)malloc(4000*sizeof(Game));
+    String entrada, cabecalho;
+    int jogos = 0;
+    fscanf(arq, " %[^\n]", cabecalho.str); 
+    while (fscanf(arq, " %[^\n]", entrada.str) != EOF) {
+        entrada.str[strcspn(entrada.str, "\r\n")] = '\0';
+        String array[14];
+        String aux;
+        int contador = 0, auxPos = 0;
+        bool aspas = false;
+        for (int i = 0; i < strlen(entrada.str); i++) {
+            char c = entrada.str[i];
+            if (c == '"') {
+                aspas = !aspas;
+            } else if (c == ',' && !aspas) {
+                aux.str[auxPos] = '\0';
+                strcpy(array[contador].str, aux.str);
+                contador++;
+                strcpy(aux.str, "");
+                auxPos = 0;
+            } else {
+                aux.str[auxPos] = c;
+                auxPos++;
+            }
+        }
+        aux.str[auxPos] = '\0';
+        strcpy(array[contador].str, aux.str);
+        settar(&game[jogos], array);
+        jogos++;
     }
     fclose(arq);
-    free(entrada);
-    char *busca = (char*)malloc(100*sizeof(char));
-    scanf("%s",busca);
-    while(!my_strcmp(busca, "FIM")) {
+    char busca[100];
+    scanf("%s", busca);
+    while (strcmp(busca, "FIM") != 0) {
         int idBusca = atoi(busca);
-        for(int i = 0; i < jogos; i++) {
-            if(idBusca == (game + i)->id) {
-                imprimir(game + i);
+        for (int i = 0; i < jogos; i++) {
+            if (idBusca == game[i].id) {
+                imprimir(&game[i]);
                 i = jogos;
             }
         }
-        scanf("%s",busca);
-    }
-    free(busca);
-    for(int i = 0; i < jogos; i++) {
-        free((game + i)->nome);
-        free((game + i)->data);
-        free((game + i)->idiomas);
-        free((game + i)->empresasPublicacao);
-        free((game + i)->empresasEstudios);
-        free((game + i)->categorias);
-        free((game + i)->generos);
-        free((game + i)->tags);
+        scanf("%s", busca);
     }
     free(game);
     return 0;
