0368f1cdc4a84d70aa4cdce4990de6b662834815
bernardoheronville <bernardoheronville@gmail.com>
2025-10-22 12:06:45 -0300

    Resolvendo construtor da matriz

--- segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/matriz/java/Main.java
+++ segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/matriz/java/Main.java
@@ -1,6 +1,6 @@
 class Celula {
-    private int elemento;
-    private Celula sup, inf, prox, ant;
+    public int elemento;
+    public Celula sup, inf, prox, ant;
 
     public Celula() {
         this.elemento = 0;
@@ -26,7 +26,27 @@ class Matriz {
         this(3, 3);
     }
     public Matriz(int l, int c) {
-        
+        this.linha = l;
+        this.coluna = c;
+        this.inicio = new Celula();
+        Celula tmp = inicio;
+        for(int i = 1; i < c; i++, tmp = tmp.prox) {
+            tmp.prox = new Celula();
+            tmp.prox.ant = tmp;
+        }
+        Celula col = inicio, linha = inicio;
+        for(int i = 1; i < l; i++) {
+            linha.inf = new Celula();
+            linha.inf.sup = linha;
+            linha = linha.inf;
+            col = linha;
+            for(int j = 1; j < c; j++, col = col.prox) {
+                col.prox = new Celula();
+                col.prox.ant = col;
+                col.prox.sup = col.sup.prox;
+                col.prox.sup.inf = col.prox;
+            }
+        }
     }
 }
 
