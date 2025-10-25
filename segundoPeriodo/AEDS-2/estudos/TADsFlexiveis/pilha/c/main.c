e5738ddc17c7773eef86d91438c59a1060e67e0b
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-10-25 18:44:24 -0300

    Treinando pilha flexivel metodos adicionados em c e java

--- segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/pilha/c/main.c
+++ segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/pilha/c/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct Celula {
     int elemento;
@@ -49,6 +50,63 @@ void mostrar(Pilha *pilha) {
     printf("\n");
 }
 
+int soma(Pilha *pilha) {
+    int soma = 0;
+    for(Celula *i = pilha->topo; i != NULL; i = i->prox) {
+        soma += i->elemento;
+    }
+    return soma;
+}
+
+int somaRecAux(Celula *i) {
+    if(i == NULL) return 0;
+    else return i->elemento + somaRecAux(i->prox);
+}
+int somaRec(Pilha *pilha) {
+    return somaRecAux(pilha->topo);
+}
+
+int maior(Pilha *pilha) {
+    int maior = INT_MIN;
+    for(Celula *i = pilha->topo; i != NULL; i = i->prox) {
+        if(i->elemento > maior) {
+            maior = i->elemento;
+        }
+    }
+    return maior;
+}
+
+int maiorRecAux(Celula *i, int maior) {
+    if(i == NULL) return maior;
+    else if(i->elemento > maior) return maiorRecAux(i->prox, i->elemento);
+    else return maiorRecAux(i->prox, maior);
+}
+int maiorRec(Pilha *pilha) {
+    return maiorRecAux(pilha->topo, INT_MIN);
+}
+
+void mostrarInvertidoRecAux(Celula *i) {
+    if(i != NULL) {
+        mostrarInvertidoRecAux(i->prox);
+        printf("%d ", i->elemento);
+    }
+}
+void mostrarInvertidoRec(Pilha *pilha) {
+    mostrarInvertidoRecAux(pilha->topo);
+    printf("\n");
+}
+
+void mostrarRecAux(Celula *i) {
+    if(i != NULL) {
+        printf("%d ", i->elemento);
+        mostrarRecAux(i->prox);
+    }
+}
+void mostrarRec(Pilha *pilha) {
+    mostrarRecAux(pilha->topo);
+    printf("\n");
+}
+
 void freePilha(Pilha *pilha) {
     Celula *i = pilha->topo; 
     while(i != NULL) {
@@ -67,6 +125,15 @@ int main() {
     mostrar(pilha); // 7 5 3
     printf("Elemento %d removido\n", remover(pilha));
     mostrar(pilha); // 5 3
+    inserir(pilha, 3);
+    inserir(pilha, 5);
+    inserir(pilha, 7);
+    printf("A soma dos elementos da pilha eh %d\n", soma(pilha)); // 23
+    printf("A soma dos elementos da pilha eh %d\n", somaRec(pilha)); // 23
+    printf("O maior elemento da pilha eh %d\n", maior(pilha)); // 7
+    printf("O maior elemento da pilha eh %d\n", maiorRec(pilha)); // 7
+    mostrarRec(pilha);
+    mostrarInvertidoRec(pilha); // 3 5 3 5 7
     freePilha(pilha);
     return 0;
 }
--- segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/pilha/java/Main.java
+++ segundoPeriodo/AEDS-2/estudos/TADsFlexiveis/pilha/java/Main.java
@@ -38,6 +38,86 @@ class Pilha {
         return resp;
     }
 
+    public int soma() {
+        int count = 0;
+        for(Celula i = topo; i != null; i = i.prox) {
+            count += i.elemento;
+        }
+        return count;
+    }
+
+    public int somaRec() {
+        return somaRec(topo);
+    }
+    private int somaRec(Celula i) {
+        if (i == null) return 0;
+        return i.elemento + somaRec(i.prox);
+    }
+
+    public int maiorElemento() {
+        int maior = Integer.MIN_VALUE;
+        for(Celula i = topo; i != null; i = i.prox) {
+            if(i.elemento > maior) {
+                maior = i.elemento;
+            }
+        }
+        return maior;
+    }
+
+    public int maiorElementoRec() {
+        return maiorElementoRec(topo, Integer.MIN_VALUE);
+    }
+    private int maiorElementoRec(Celula i, int maior) {
+        if (i == null) {
+            return maior;
+        }     
+        else {
+            if (i.elemento > maior) {
+                maior = i.elemento;
+            }
+            return maiorElementoRec(i.prox, maior);
+        }
+    }
+
+    public void mostrarRec() {
+        mostrarRec(topo);
+        System.out.printf("\n");
+    }
+    private void mostrarRec(Celula i) {
+        if(i != null) {
+            System.out.printf("%d ", i.elemento);
+            mostrarRec(i.prox);
+        } 
+    }
+
+    public void mostrarRecInvertido() {
+        mostrarRecInvertido(topo);
+        System.out.printf("\n");
+    }
+    private void mostrarRecInvertido(Celula i) {
+        if(i != null) {
+            mostrarRecInvertido(i.prox);
+            System.out.printf("%d ", i.elemento);
+        } 
+    }
+
+    public int tamanho() {
+        int count = 0;
+        for(Celula i = topo; i != null; i = i.prox) {
+            count++;
+        }
+        return count;
+    }
+
+    public void mostrarInvertido() {
+        for(int i = tamanho() - 1; i >= 0; i--) {
+            Celula tmp = topo;
+            for(int j = 0; j < i; j++, tmp = tmp.prox);
+            System.out.printf("%d ", tmp.elemento);
+        }
+        System.out.printf("\n");
+    }
+
     public void mostrar() {
         for(Celula i = topo; i != null; i = i.prox) {
             System.out.printf("%d ", i.elemento);
@@ -55,5 +135,15 @@ public class Main {
         pilha.mostrar(); // 7 5 3
         System.out.println("Elemento " + pilha.remover() + " removido");
         pilha.mostrar(); // 5 3
+        pilha.inserir(3);
+        pilha.inserir(5);
+        pilha.inserir(7);
+        System.out.println("A soma dos elementos da pilha eh " + pilha.soma()); // 23
+        System.out.println("A soma dos elementos da pilha eh " + pilha.somaRec()); // 23
+        System.out.println("O maior elemento da pilha eh " + pilha.maiorElemento()); // 7
+        System.out.println("O maior elemento da pilha eh " + pilha.maiorElementoRec()); // 7
+        pilha.mostrarRec(); // 7 5 3 5 3
+        pilha.mostrarRecInvertido(); // 3 5 3 5 7
+        pilha.mostrarInvertido(); // 3 5 3 5 7
     }
 }
