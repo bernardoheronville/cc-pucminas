eaf04530c710f1b4672e27d8445abff9b5c3fde2
Bernardo Barbosa Heronville <bernardoheronville@gmail.com>
2025-10-26 17:14:40 -0300

    Iniciando tp06

---
+++ segundoPeriodo/AEDS-2/tps/tp06/ex01/Main.java
@@ -0,0 +1,442 @@
+import java.util.Scanner;
+import java.io.*;
+
+// Classe Celula.
+class Celula {
+    public Game elemento;
+    public Celula prox;
+
+    // Construtores da classe Celula.
+    public Celula() {
+        elemento = new Game();
+        prox = null;
+    }
+    public Celula(Game game) {
+        elemento = game;
+        prox = null;
+    }
+}
+
+// Classe Game.
+class Game {
+    private int id;
+    private String nome;
+    private String data;
+    private int jogadores;
+    private float preco;
+    private String idiomas[];
+    private int notaEspecial;
+    private float notaUsuario;
+    private int conquistas;
+    private String empresasPublicacao[];
+    private String empresasEstudios[];
+    private String categorias[];
+    private String generos[];
+    private String tags[];
+
+    // Construtor da classe Game.
+    public Game() {
+        this.id = 0;
+        this.nome = "";
+        this.data = "01/01/0000";
+        this.jogadores = 0;
+        this.preco = 0.0f;
+        this.idiomas = new String[0];
+        this.notaEspecial = -1;
+        this.notaUsuario = -1.0f;
+        this.conquistas = 0;
+        this.empresasPublicacao = new String[0];
+        this.empresasEstudios = new String[0];
+        this.categorias = new String[0];
+        this.generos = new String[0];
+        this.tags = new String[0];
+    }
+
+    // Metodos setters.
+    public void setID(String id) {
+        if (id != null) {
+            this.id = Integer.parseInt(id);
+        }
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setData(String data) {
+        if (data == null) {
+            this.data = "01/01/0000";
+            return;
+        }
+        String mes = "", dia = "", ano = "";
+        for (int i = 0; i < 3; i++) {
+            mes += data.charAt(i);
+        }
+        if (data.charAt(5) == ',') {
+            dia = "0" + data.charAt(4);
+            for (int i = 7; i < data.length(); i++) {
+                ano += data.charAt(i);
+            }
+        } else {
+            dia += data.charAt(4);
+            dia += data.charAt(5);
+            for (int i = 8; i < data.length(); i++) {
+                ano += data.charAt(i);
+            }
+        }
+        switch (mes) {
+            case "Jan": mes = "01"; break;
+            case "Feb": mes = "02"; break;
+            case "Mar": mes = "03"; break;
+            case "Apr": mes = "04"; break;
+            case "May": mes = "05"; break;
+            case "Jun": mes = "06"; break;
+            case "Jul": mes = "07"; break;
+            case "Aug": mes = "08"; break;
+            case "Sep": mes = "09"; break;
+            case "Oct": mes = "10"; break;
+            case "Nov": mes = "11"; break;
+            case "Dec": mes = "12"; break;
+            default: mes = "01"; break;
+        }
+        this.data = dia + "/" + mes + "/" + ano;
+    }
+    public void setJogadores(String jogadores) {
+        String aux = "";
+        for(int i = 0; i < jogadores.length(); i++) {
+            if(jogadores.charAt(i) >= '0' && jogadores.charAt(i) <= '9') {
+                aux += jogadores.charAt(i);
+            }
+        }
+        this.jogadores = Integer.parseInt(aux);
+    }
+    public void setPreco(String preco) {
+        if(preco.equals("Free to Play")) {
+            this.preco = 0.0f;
+        }
+        else {
+            this.preco = Float.parseFloat(preco);
+        } 
+    }
+    public void setIdiomas(String idiomas) {
+        this.idiomas = formatar(idiomas);
+    }
+    public void setNotaEspecial(String notaEspecial) {
+        if(notaEspecial == null) {
+            this.notaEspecial = -1; 
+        } else {
+            this.notaEspecial = Integer.parseInt(notaEspecial);
+        }
+    }
+    public void setNotaUsuario(String notaUsuario) {
+        if(notaUsuario == null || notaUsuario.equals("tbd")) {
+            this.notaUsuario = -1.0f; 
+        } else {
+            this.notaUsuario = Float.parseFloat(notaUsuario);
+        }
+    }
+    public void setConquistas(String conquistas) {
+        if(conquistas == null) {
+            this.conquistas = 0;
+        } else {
+            this.conquistas = Integer.parseInt(conquistas);
+        } 
+    }
+    public void setEmpresasPublicacao(String empresasPublicacao) {
+        this.empresasPublicacao = formatar(empresasPublicacao);
+    }
+    public void setEmpresasEstudios(String empresasEstudios) {
+        this.empresasEstudios = formatar(empresasEstudios);
+    }
+    public void setCategorias(String categorias) {
+        this.categorias = formatar(categorias);
+    }
+    public void setGeneros(String generos) {
+        this.generos = formatar(generos);
+    }
+    public void setTags(String tags) {
+       this.tags = formatar(tags);
+    }
+
+    // Metodos getters.
+    public int getID() { return id; };
+    public String getNome() { return nome; };
+    public String getData() { return data; };
+    public int getJogadores() { return jogadores; };
+    public float getPreco() { return preco; }
+    public String[] getIdiomas() { return idiomas; };
+    public int getNotaEspecial() { return notaEspecial; };
+    public float getNotaUsuario() { return notaUsuario; }
+    public int getConquistas() { return conquistas; };
+    public String[] getEmpresasPublicacao() { return empresasPublicacao; };
+    public String[] getEmpresasEstudios() { return empresasEstudios; };
+    public String[] getCategorias() { return categorias; };
+    public String[] getGeneros() { return generos; };
+    public String[] getTags() { return tags; };
+
+    // Funcao que formata as strings.
+    public String[] formatar(String entrada) {
+        if (entrada == null || entrada.equals("[]")) {
+            return new String[0];
+        }
+        int virgulas = 1;
+        for(int i = 0; i < entrada.length(); i++) {
+            char c = entrada.charAt(i);
+            if(c == ',') {
+                virgulas++;
+            }
+        }
+        String resp[] = new String[virgulas];
+        String aux = "";
+        int contador = 0;
+        for(int i = 0; i < entrada.length(); i++) {
+            char c = entrada.charAt(i);
+            if(c == ',') {
+                resp[contador] = limparString(aux);
+                contador++;
+                aux = "";
+            } else {
+                if(!(c == '[' || c == ']')) {
+                    aux += c;
+                }
+            }
+        }
+        resp[contador] = limparString(aux);
+        return resp;
+    }
+    // Funcao aulixiar para formatar os espacos.
+    private String limparString(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+        int inicio = 0;
+        int fim = str.length() - 1;
+
+        while (inicio < str.length() && str.charAt(inicio) <= ' ') {
+            inicio++;
+        }
+
+        while (fim >= inicio && str.charAt(fim) <= ' ') {
+            fim--;
+        }
+        if (inicio > fim) {
+            return "";
+        }
+        if ((fim - inicio + 1) >= 2 && str.charAt(inicio) == '\'' && str.charAt(fim) == '\'') {
+            inicio++;
+            fim--;
+        }
+        String resultado = "";
+        for (int i = inicio; i <= fim; i++) {
+            resultado += str.charAt(i);
+        }
+        return resultado;
+    }
+
+    // Funcao para mostrar os atributos da classe.
+    private String auxiliarMostrar(String array[]) {
+        String result = "[";
+        if (array != null) {
+            for(int i = 0; i < array.length; i++) {
+                result += array[i];
+                if(i < array.length - 1) {
+                    result += ", ";
+                }
+            }
+        }
+        result += "]";
+        return result;
+    }
+    @Override 
+    public String toString() {
+        return ("=> " + id + " ## " + nome + " ## " + data + " ## " + jogadores + " ## " + preco + " ## " + auxiliarMostrar(idiomas) + " ## " + notaEspecial
+                + " ## " + notaUsuario + " ## " + conquistas + " ## " + auxiliarMostrar(empresasPublicacao) + " ## " + auxiliarMostrar(empresasEstudios) + " ## " + auxiliarMostrar(categorias)
+                + " ## " + auxiliarMostrar(generos) + " ## " + auxiliarMostrar(tags) + " ##");
+    }
+}
+
+// Classe Lista.
+class Lista {
+    public Celula primeiro;
+    public Celula ultimo;
+
+    // Construtor da classe Lista.
+    public Lista() {
+        primeiro = new Celula();
+        ultimo = primeiro;
+    }
+
+    // Procedimentos de insercao da Lista.
+    public void inserirInicio(Game game) {
+        primeiro.elemento = game;
+        Celula tmp = new Celula();
+        tmp.prox = primeiro;
+        primeiro = tmp;
+        tmp = null;
+    }
+    public void inserirFim(Game game) {
+        ultimo.prox = new Celula(game);
+        ultimo = ultimo.prox;
+    }
+    public void inserir(Game game, int pos) {
+        int tam = tamanho();
+        if(pos < 0 || pos > tam) {
+            throw new IllegalArgumentException("Posicao invalida");
+        }
+        else if(pos == 0) inserirInicio(game);
+        else if(pos == tam) inserirFim(game);
+        else {
+            Celula nova = new Celula(game);
+            Celula tmp = primeiro.prox;
+            for(int i = 0; i < pos - 1; i++, tmp = tmp.prox);
+            nova.prox = tmp.prox;
+            tmp.prox = nova;
+            tmp = nova = null;
+        }
+    }
+
+    // Funcoes de remocao da Lista.
+    public Game removerInicio() {
+        if(primeiro == ultimo) {
+            throw new IllegalArgumentException("Lista vazia");
+        }
+        Celula tmp = primeiro.prox;
+        primeiro.prox = tmp.prox;
+        tmp.prox = null;
+        Game resp = tmp.elemento;
+        tmp = null;
+        return resp;
+    }
+    public Game removerFim() {
+        if(primeiro == ultimo) {
+            throw new IllegalArgumentException("Lista vazia");
+        }
+        Celula i;
+        for(i = primeiro.prox; i.prox != ultimo; i = i.prox);
+        Game resp = ultimo.elemento;
+        ultimo = i;
+        i = ultimo.prox;
+        ultimo.prox = null;  
+        i = null;
+        return resp;
+    }
+    public Game remover(int pos) {
+        int tam = tamanho();
+        Game resp;
+        if(primeiro == ultimo) {
+            throw new IllegalArgumentException("Lista vazia");
+        }
+        else if(pos < 0 || pos >= tam) {
+            throw new IllegalArgumentException("Posicao invalida");
+        }
+        else if(pos == 0) resp = removerInicio();
+        else if(pos == tam - 1) resp = removerFim();
+        else {
+            Celula tmp = primeiro.prox, remover;
+            for(int i = 0; i < pos - 1; i++, tmp = tmp.prox);
+            remover = tmp.prox;
+            tmp.prox = remover.prox;
+            remover.prox = null;
+            resp = remover.elemento;
+            remover = null;
+        }
+        return resp;
+    }
+
+    // Funcao que retorna o tamanho da Lista.
+    public int tamanho() {
+        int count = 0;
+        for(Celula i = primeiro.prox; i != null; i = i.prox) {
+            count++;
+        }
+        return count;
+    }
+
+    // Precedimento que mostrar os elementos da Lista.
+    public void mostrar() {
+        for(Celula i = primeiro.prox; i != null; i = i.prox) {
+            System.out.println(i.elemento.toString());
+        }
+    }
+}
+
+public class Main {
+    // Procedimento auxiliar do metodo settar.
+    public static void settar(Game game, String array[]) {
+        game.setID(array[0]);
+        game.setNome(array[1]);
+        game.setData(array[2]);
+        game.setJogadores(array[3]);
+        game.setPreco(array[4]);
+        game.setIdiomas(array[5]);
+        game.setNotaEspecial(array[6]);
+        game.setNotaUsuario(array[7]);
+        game.setConquistas(array[8]);
+        game.setEmpresasPublicacao(array[9]);
+        game.setEmpresasEstudios(array[10]);
+        game.setCategorias(array[11]);
+        game.setGeneros(array[12]);
+        game.setTags(array[13]);
+    }
+
+    // Funcao para retornar true or false se as string sao iguais.
+    public static boolean my_strcmp(String str1, String str2) {
+        boolean resp = true;
+        if(str1.length() == str2.length()) {
+            for(int i = 0; i < str1.length(); i++) {
+                if(str1.charAt(i) != str2.charAt(i)) {
+                    resp = false;
+                    i = str1.length();
+                }
+            }
+        }
+        else {
+            resp = false;
+        }
+        return resp;
+    }
+
+    // Main
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        File arq = new File("../tmp/games.csv");
+        Scanner scfile = new Scanner(arq);
+        Lista lista = new Lista();
+        scfile.nextLine();
+        while(scfile.hasNextLine()) { 
+            String entrada = scfile.nextLine();
+            String array[] = new String[14]; 
+            String aux = "";
+            int contador = 0;
+            boolean aspas = false;
+            for (int i = 0; i < entrada.length(); i++) {
+                char c = entrada.charAt(i);
+                if (c == '"') {
+                    aspas = !aspas;
+                } else if (c == ',' && !aspas) {
+                    array[contador] = aux;
+                    contador++;
+                    aux = "";
+                } else {
+                    aux += c;
+                }
+            }
+            array[contador] = aux;
+            Game game = new Game();
+            settar(game, array);
+            lista.inserirFim(game);
+        }
+        // Testando com a entrada do tp04, quando receber a entrada do tp06 arrumar.
+        String busca = sc.nextLine();
+        while(!busca.equals("FIM")) {
+            int idBusca = Integer.parseInt(busca);
+            for(Celula i = lista.primeiro.prox; i != null; i = i.prox) {
+                if(idBusca == i.elemento.getID()) {
+                    System.out.println(i.elemento.toString());
+                    i = lista.ultimo;
+                }
+            }
+            busca = sc.nextLine();
+        }
+        sc.close();
+        scfile.close();
+    }
+}
